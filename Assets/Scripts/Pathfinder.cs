74a1c70f6bf9beab27f55395a63c4630af5e0ade
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-07-19 18:58:45 +0300

    Add enemy moving

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> waypoins;
+    int waypointIndex = 0;
+    void Start()
+    {
+        waypoins = waveConfig.GetWaypoints();
+        transform.position = waypoins[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if(waypointIndex < waypoins.Count)
+        {
+            Vector3 targetPosition = waypoins[waypointIndex].position;
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, delta);
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
