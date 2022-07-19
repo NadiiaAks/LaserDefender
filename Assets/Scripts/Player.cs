0266cca9f9167202f0f70ffee1d97159c063624c
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-07-19 17:23:21 +0300

    Add Bounds

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,16 +6,34 @@ using UnityEngine.InputSystem;
 public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 5f;
+
     private Vector2 rawInput;
+    private Vector2 minBouns;
+    Vector2 maxBouns;
+
+    private void Start()
+    {
+        InitBounds();
+    }
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBouns = mainCamera.ViewportToWorldPoint(new Vector2(0, 0));
+        maxBouns = mainCamera.ViewportToWorldPoint(new Vector2(1, 1));  
+    }
+
     private void Move()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2();
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBouns.x, maxBouns.x);
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBouns.y, maxBouns.y);
+        transform.position = newPos;
     }
 
     private void OnMove(InputValue value)
