7cb0af502cf97c23bf1781b20259b1a0ff4a22cb
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-08-04 17:31:47 +0300

    Edit level manager: add next scene and index for last scene

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -5,24 +5,52 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] float sceneLoadDeley = 2f;
+    [SerializeField] int scoreForNextLvl = 100;
+
+    ScoreKeeper scoreKeeper;
+    int currentScene;
+    int lastScene;
+
+    private void Awake()
+    {
+        scoreKeeper =  FindObjectOfType<ScoreKeeper>();
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+        lastScene = SceneManager.sceneCountInBuildSettings;
+    }
+
+    private void Update()
+    {
+        NextLevel();
+    }
     public void LoadGame()
     {
+        scoreKeeper.ResetScore();
         SceneManager.LoadScene(1);
     }
 
     public void LoadMainManu()
     {
+        scoreKeeper.ResetScore();
         SceneManager.LoadScene(0);
     }
 
     public void LoadGameOver()
     {
-        StartCoroutine(WaitAndLoad(2, sceneLoadDeley));
+        Debug.Log("Game Over" + lastScene);
+        StartCoroutine(WaitAndLoad(3, sceneLoadDeley));
     }
 
     public void ExitGame()
     {
         Application.Quit();
+    }
+
+    void NextLevel()
+    {
+        if (scoreKeeper.GetScore() >= scoreForNextLvl && currentScene != lastScene)
+        {
+            StartCoroutine(WaitAndLoad(currentScene + 1, sceneLoadDeley));
+        }
     }
 
     IEnumerator WaitAndLoad(int Scene, float delay)
