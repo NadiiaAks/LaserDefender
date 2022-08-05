ac3db91221c696634a12b5d8b374ac28f5c00522
Nadiia Aksonova <nadizarubina11@gmail.com>
2022-08-05 12:50:45 +0300

    Add boss and his moving

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -7,19 +7,56 @@ public class Boss : MonoBehaviour
     [SerializeField] private float bossSpeed;
     [SerializeField] private float delay = 2f;
 
-    [SerializeField] private Vector3 rightPoint;
-    [SerializeField] private Vector3 leftPoint;
+    [SerializeField] private Vector2 rightPoint;
+    [SerializeField] private Vector2 leftPoint;
+    [SerializeField] private Vector2 downPoint;
 
-    private Vector3 move;
+    private bool isRight;
+    private bool isLeft = true;
+    private bool isDown;
 
-    void Update()
+    private void Update()
     {
         Move();
     }
 
     private void Move()
     {
+        if (isRight)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, leftPoint, bossSpeed * Time.deltaTime);
+        }
+       
+        if (isLeft)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, downPoint, bossSpeed * Time.deltaTime);
+        }
 
+        if (isDown)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, rightPoint, bossSpeed * Time.deltaTime);
+        }
+
+        if (transform.position.x == rightPoint.x)
+        {
+            isRight = true;
+            isLeft = false;
+            isDown = false;
+        }
+
+        if (transform.position.x == leftPoint.x)
+        {
+            isLeft = true;
+            isRight = false;
+            isDown = false;
+        }
+
+        if(transform.position.y == downPoint.y)
+        {
+            isLeft = false;
+            isRight = false;
+            isDown = true;
+        }
 
     }
 
